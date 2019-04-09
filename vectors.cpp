3930cea31dfea099b4c0f2aa83cd48f5e0ca4711
Marcus Gelderie <redmnic@gmail.com>
2019-04-09 07:03:18 +0200

    Force lowest bit of _start address to 1
    Use the .thumb_func directive to tell toolchain to use an odd address
    for the _start function.

--- vectors.cpp
+++ vectors.cpp
@@ -5,5 +5,5 @@ extern "C" void _start(void);
 
 ExceptionHandler vectors[2] __attribute__((section(".vectable"))) = {
         (ExceptionHandler)0x20001000,
-        (ExceptionHandler)(1 + (unsigned int)&_start),
+        (ExceptionHandler)&_start,
 };
